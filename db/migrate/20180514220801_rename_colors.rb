5e755d2a4a4da213fd5074b36ad98d102349ae31
Giles Lewey <gileslewey@gmail.com>
2018-05-17 22:53:46 -0700

    deleted redundant migrations

--- db/migrate/20180514220801_rename_colors.rb
+++
@@ -1,6 +0,0 @@
-
-class RenameColors < ActiveRecord::Migration[5.1]
-  def change
-    rename_column :products, :color, :grade
-  end
-end
--- db/migrate/20180514221251_update_color.rb
+++
@@ -1,5 +0,0 @@
-class UpdateColor < ActiveRecord::Migration[5.1]
-  def change
-    rename_column :products, :colour, :grade
-  end
-end
