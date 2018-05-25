414c7393cb71f49128c8cf63674655b4b1fa241b
Giles Lewey <gileslewey@gmail.com>
2018-05-25 13:42:48 -0700

    design fixes product page

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,11 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-  Product.create(name: "1929 Indian Head Nickle", description:
-    "Denver Minted", image_url: "indian.jpg", Grade: "Uncirculated", Price: "49")
+  Product.create(name: "1929 Indian Head Nickle", description: "Denver Minted", image_url: "indian.jpg", grade: "Uncirculated", price: "49")
 
-  Product.create(name: "1879 Morgan Silver Dollar", description:
-    "Carson City minted", image_url: "morgan.jpg", Grade: "Uncirculated", Price: "70")
+  Product.create(name: "1879 Morgan Silver Dollar", description: "Carson City minted", image_url: "morgan.jpg", grade: "Uncirculated", price: "70")
 
-  Product.create(name: "1955 Double Die Cent", description:
-    "Philadelphia Minted", image_url: "ddpenny.jpg", Grade: "Very Good", Price: "700")
+  Product.create(name: "1955 Double Die Cent", description: "Philadelphia Minted", image_url: "ddpenny.jpg", grade: "Very Good", price: "700")
