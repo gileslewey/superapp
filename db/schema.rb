d3127605a42b169e7a9e942fda92a52d5504b42d
Giles Lewey <gileslewey@gmail.com>
2018-06-01 09:13:49 -0700

    4.5 changed grade to quality

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180517234527) do
+ActiveRecord::Schema.define(version: 20180601160611) do
 
   create_table "orders", force: :cascade do |t|
     t.integer "user_id"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 20180517234527) do
     t.datetime "updated_at", null: false
     t.string "grade"
     t.integer "price"
+    t.string "quality"
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-  Product.create(name: "1929 Indian Head Nickle", description: "Denver Minted", image_url: "indian.jpg", grade: "Uncirculated", price: "49")
+  Product.create(name: "1929 Indian Head Nickle", description: "Denver Minted", image_url: "indian.jpg", quality: "Uncirculated", price: "49")
 
-  Product.create(name: "1879 Morgan Silver Dollar", description: "Carson City minted", image_url: "morgan.jpg", grade: "Uncirculated", price: "70")
+  Product.create(name: "1879 Morgan Silver Dollar", description: "Carson City minted", image_url: "morgan.jpg", quality: "Uncirculated", price: "70")
 
-  Product.create(name: "1955 Double Die Cent", description: "Philadelphia Minted", image_url: "ddpenny.jpg", grade: "Very Good", price: "700")
+  Product.create(name: "1955 Double Die Cent", description: "Philadelphia Minted", image_url: "ddpenny.jpg", quality: "Very Good", price: "700")
