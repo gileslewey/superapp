0df129b415976df994509e1e72a741b514a1eec9
Giles Lewey <gileslewey@gmail.com>
2018-06-01 12:12:57 -0700

    4.6 seeded orders

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,10 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+  Order.create(user_id: 1, product_id: 1, total: 10.0)
+  
+  Order.create(user_id: 1, product_id: 2, total: 5.0)
+
   Product.create(name: "1929 Indian Head Nickle", description: "Denver Minted", image_url: "indian.jpg", quality: "Uncirculated", price: "49")
 
   Product.create(name: "1879 Morgan Silver Dollar", description: "Carson City minted", image_url: "morgan.jpg", quality: "Uncirculated", price: "70")
