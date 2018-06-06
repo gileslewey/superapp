c5dbc88534a982b6f3138382ce4d16137fa9484f
Giles Lewey <gileslewey@gmail.com>
2018-06-06 04:37:01 -0500

    4.3 generated mailer code

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,8 @@ Rails.application.routes.draw do
   get 'simple_pages/landing_page'
 
   root 'simple_pages#landing_page'
+
+  post 'simple_pages/thank_you'
   resources :orders, only: [:index, :show, :create, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
