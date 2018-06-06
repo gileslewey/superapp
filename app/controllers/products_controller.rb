a71d455a5a729b73831c2f42f6ff2063dba2fa2a
Giles Lewey <gileslewey@gmail.com>
2018-06-06 01:51:01 -0500

    5.2 forms

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,8 +4,14 @@ class ProductsController < ApplicationController
   # GET /products
   # GET /products.json
   def index
-    @products = Product.all
+    if params[:q]
+      search_term = params[:q]
+      @products = Product.search(search_term)
+    else
+      @products = Product.all
+    end
   end
+
 
   # GET /products/1
   # GET /products/1.json
@@ -61,7 +67,7 @@ class ProductsController < ApplicationController
     end
   end
 
-  private
+
     # Use callbacks to share common setup or constraints between actions.
     def set_product
       @product = Product.find(params[:id])
