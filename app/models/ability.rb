b5b173af62d52da70963c4518dcc57edb7439615
Giles Lewey <gileslewey@gmail.com>
2018-06-22 23:52:49 -0500

    5.10 added admin flag, added destroy to comments, set admin permissions in Ability model

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,11 +2,16 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    user ||= User.new # guest user (not logged in)
-    can :manage, User, id: user.id
+    user ||= User.new #guest user (not logged in)
+    if user.admin?
+      can :manage, :all
+    else
+      can :manage, User, id: user.id
+    end
   end
-
 end
+
+
   #def initialize(user)
     # Define abilities for the passed in user here. For example:
     #
